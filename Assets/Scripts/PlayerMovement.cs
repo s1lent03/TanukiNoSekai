50a8f7ef2015351f4422f5d59bd67a9ed9155d43
aura <31188551+headrow@users.noreply.github.com>
2023-10-20 10:49:36 +0100

    Player Movement V3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float speed;
+    public float rotationSpeed;
     public float gravity;
 
     [Header("Ground Check")]
@@ -16,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     private CharacterController controller;
     private PlayerInput playerInput;
-    private Vector3 velocity;
+    private float height;
     private bool isGrounded;
 
     void Start()
@@ -29,18 +30,22 @@ public class PlayerMovement : MonoBehaviour
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0)
+        Vector2 move = playerInput.actions["Move"].ReadValue<Vector2>();
+        Vector3 direction = new Vector3(move.x, 0, move.y);
+        direction.Normalize();
+
+        if (!isGrounded)
         {
-            velocity.y = -2f;
+            height += gravity * Time.deltaTime;
+            direction.y = height;
         }
 
-        Vector2 move = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector3 direction = transform.right * move.x + transform.forward * move.y;
-        Debug.Log(direction);
-
-        controller.Move(direction * speed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        
+        if (direction != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
