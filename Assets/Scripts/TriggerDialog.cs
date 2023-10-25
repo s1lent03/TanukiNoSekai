f0894722995ce5620cd7cdcb6edf65cd96b7d85a
headrow <31188551+headrow@users.noreply.github.com>
2023-10-25 22:16:54 +0100

    Dialogo V1.1
    o tween nao quer dar direito, nice

--- Assets/Scripts/TriggerDialog.cs
+++ Assets/Scripts/TriggerDialog.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TriggerDialog : MonoBehaviour
 {
+    [Header("Objects")]
     public GameObject dialogMenu;
+
+    private Vector3 menuPosition;
+    private Vector3 menuOutOfSight;
+
+    private void Awake()
+    {
+        menuPosition = dialogMenu.transform.position;
+        menuOutOfSight = menuPosition - new Vector3(0, 400, 0);
+
+        Debug.Log(menuPosition);
+
+        dialogMenu.transform.position = menuOutOfSight;
+        dialogMenu.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             dialogMenu.SetActive(true);
+            dialogMenu.transform.DOMove(menuPosition, 1).SetEase(Ease.OutCubic);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
+            dialogMenu.transform.position = menuOutOfSight;
             dialogMenu.SetActive(false);
+        }
     }
 }
