c72472622dadeaa8f20ee482293945c1f5df6641
headrow <31188551+headrow@users.noreply.github.com>
2023-11-12 19:49:20 +0000

    Revert "Navmesh e NPC Movement Final"
    This reverts commit a3d4396ca1a9049b21752d7dd4fbedcaf7cc5f53.

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -8,6 +8,7 @@ public class NPCMovement : MonoBehaviour
     public float range;
 
     private NavMeshAgent navMeshAgent;
+
     private bool inCoroutine = false;
     private Vector3 target = Vector3.zero;
 
@@ -26,19 +27,17 @@ public class NPCMovement : MonoBehaviour
     {
         inCoroutine = true;
 
-        while (target == Vector3.zero)
+        if (RandomPoint(transform.position, range, out target))
         {
-            yield return new WaitForSeconds(0.01f);
-            RandomPoint(transform.position, range, out target);
-            navMeshAgent.SetDestination(target);
+            Debug.DrawLine(transform.position, target, Color.red);
+            navMeshAgent.destination = target;
         }
 
-        while ((transform.position.x != target.x) && (transform.position.z != target.z))
+        while ((Mathf.Round(transform.position.x) != target.x) && (Mathf.Round(transform.position.z) != target.z))
         {
             yield return new WaitForSeconds(0.01f);
         }
 
-        target = Vector3.zero;
         inCoroutine = false;
     }
 
@@ -48,14 +47,12 @@ public class NPCMovement : MonoBehaviour
         {
             Vector3 randomPoint = center + Random.insideUnitSphere * range;
             NavMeshHit hit;
-
             if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
             {
                 result = hit.position;
                 return true;
             }
         }
-
         result = Vector3.zero;
         return false;
     }
